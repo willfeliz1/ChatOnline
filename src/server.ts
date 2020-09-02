8a260842bd3ce61c14432d90fd4d56e07b3598df
willfeliz1 <william_mfelizardo@hotmail.com>
2020-09-02 12:35:33 -0300

    feat: socketIO test

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,31 @@
 import express from 'express';
+import socketIO from 'socket.io';
+
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'hello world'});
+app.get('/chatOnline', (request, response) => {
+  response.sendFile(`${__dirname}/index.html`);
 });
 
-app.listen(3333, () => {
+const server = app.listen(3333, () => {
   console.log('-- Server Started on port 3333 --');
-})
+});
+
+const io = socketIO(server);
+
+io.on('connection', socket => {
+  console.log('an user is connected');
+
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+app.use(express.json());
+// app.use(routes);

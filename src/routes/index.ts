ee151219e75011c31ce2c1248781d18009f8c4cb
willfeliz1 <william_mfelizardo@hotmail.com>
2020-09-07 22:00:34 -0300

    feat: user router and controller to create user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,12 @@
 import { Router } from 'express';
+// import chatRouter from './chat.routes';
+// import sessionsRouter from '../modules/users/routes/sessions.routes';
+import usersRouter from '../modules/users/routes/users.routes';
 
 const routes = Router();
+
+routes.use('/users', usersRouter);
+// routes.use('/sessions', sessionsRouter);
+// routes.use('/chat', chatRouter);
 
 export default routes;

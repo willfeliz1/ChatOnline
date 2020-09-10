0f76d3bcbfb3ca3fb1dc4a6075ae5631d86e13a5
willfeliz1 <william_mfelizardo@hotmail.com>
2020-09-10 17:55:07 -0300

    feat: Authenticate user service with jwtToken

---
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -0,0 +1,45 @@
+import { injectable, inject } from 'tsyringe';
+import { sign } from 'jsonwebtoken';
+import User from '../entities/User';
+import IUsersRepository from '../repositoriesInterface/IUsersRepository';
+import AppError from '../../../errors/AppError';
+import authConfig from '../../../config/auth';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: User;
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('Incorrect email/password combination', 401);
+    }
+
+    // compareHash
+
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+
+    return { user, token };
+  }
+}
+
+export default AuthenticateUserService;
